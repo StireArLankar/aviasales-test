10567c7e04516778f6f041cc41805d4d2e84f0c3
StireArLankar <stire.ru@gmail.com>
2019-03-06 23:12:11 +0300

    Updates

---
+++ precache-manifest.7297778d686de4e1afbdc51403a94c62.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "55835483c304eaa8477fea2c36abba17",
+    "url": "/aviasales-test/static/media/OpenSans-Regular.55835483.woff2"
+  },
+  {
+    "revision": "f9ead45178525650c389",
+    "url": "/aviasales-test/static/css/main.f78ee74b.chunk.css"
+  },
+  {
+    "revision": "55ea38685ffdfa89e3aa",
+    "url": "/aviasales-test/static/js/runtime~main.55ea3868.js"
+  },
+  {
+    "revision": "71680640981c2de7213d",
+    "url": "/aviasales-test/static/js/2.71680640.chunk.js"
+  },
+  {
+    "revision": "3556cd0846da0cb50c0964c5d402957a",
+    "url": "/aviasales-test/static/media/S7.3556cd08.svg"
+  },
+  {
+    "revision": "bbad233ac0f7f4b5128b1bbcb4cad255",
+    "url": "/aviasales-test/static/media/SU.bbad233a.png"
+  },
+  {
+    "revision": "3326e4d74d3924ee1c882c29f5b571c0",
+    "url": "/aviasales-test/static/media/OpenSans-Bold.3326e4d7.woff2"
+  },
+  {
+    "revision": "f9ead45178525650c389",
+    "url": "/aviasales-test/static/js/main.f9ead451.chunk.js"
+  },
+  {
+    "revision": "ce3b96ab2b51255e60d2d1aae633dae3",
+    "url": "/aviasales-test/static/media/OpenSans-Regular.ce3b96ab.woff"
+  },
+  {
+    "revision": "58308bcc8dce00a629bd46305a1c5494",
+    "url": "/aviasales-test/static/media/OpenSans-Bold.58308bcc.woff"
+  },
+  {
+    "revision": "08952b029e4decbc8ef9fb553cae8cea",
+    "url": "/aviasales-test/static/media/OpenSans-SemiBold.08952b02.woff2"
+  },
+  {
+    "revision": "0cb4787114f012d2daa4645df1811808",
+    "url": "/aviasales-test/static/media/OpenSans-SemiBold.0cb47871.woff"
+  },
+  {
+    "revision": "d9fa3917d46326141afc856511bc323b",
+    "url": "/aviasales-test/static/media/aircraft.d9fa3917.svg"
+  },
+  {
+    "revision": "377d90fff0983f774a3b335da27522ff",
+    "url": "/aviasales-test/index.html"
+  }
+];
